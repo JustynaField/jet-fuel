2f4bd385e20f264b8434b381fbdcdb695b7b8823
Justyna Field <justyna.field@gmail.com>
2017-08-14 16:44:54 -0600

    basic css

--- public/script.js
+++ public/script.js
@@ -1,3 +1,15 @@
-$('button').click(function() {
-  alert("jQuery connected")
+var folderName = $('#folder-name')
+
+// $('button').click(function() {
+//   alert("jQuery connected")
+// })
+
+$('#create-folder').on('click', function() {
+  createNewFolder();
 })
+
+function createNewFolder() {
+  $('.folders-area').prepend(
+    '<div><h3>' + folderName.val() + '</h3></div>'
+  )
+}
