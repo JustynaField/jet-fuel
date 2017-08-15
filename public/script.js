9ceb110e3cc422373185cf600fe76f0712b0310f
Justyna Field <justyna.field@gmail.com>
2017-08-15 09:10:08 -0600

    toggle folders and display folder details

--- public/script.js
+++ public/script.js
@@ -1,18 +1,54 @@
 var folderName = $('#folder-name')
 
-// $('button').click(function() {
-//   alert("jQuery connected")
-// })
-
-$('#create-folder').on('click', function() {
+$('#create-folder').on('click', function(e) {
+  e.preventDefault();
   createNewFolder();
+  $('#folder-name').val('');
 })
 
-function createNewFolder() {
+const createNewFolder = () => {
   $('.folders-list').prepend(
     '<div class="card">' +
-      '<h3>' + folderName.val() + '</h3>'
-      + '<img src="icons/garbage.svg" class="garbage" />' +
+      '<h3 class="name">' + folderName.val() + '</h3>' +
     '</div>'
   )
+}
+
+$('.folders-list').on('click', '.card', function() {
+
+  const folderId = $(this).closest('.name').attr('id')
+  const target = $(this);
+
+  listFolderDetails(target);
+  $(this).addClass('selected')
+})
+
+// const showOneFolder = (selected) {
+//   $('.folders-list').each(function(index) {
+//     if ($(this).attr)
+//   })
+// }
+
+// const clearFolders = () => {
+//   $('.folders-list').empty()
+// }
+
+const listFolderDetails = (target, folderId) => {
+
+  $('.folder-details').empty();
+  $('.folders-list').siblings('.folders-list').removeClass('selected')
+
+  const folderName = $(target).closest('.card').find('h3')
+
+  $('.folder-details').append(
+      '<div>' +
+        '<p>'+ folderName +'</p>' +
+        '<p>'+ folderId +'</p>' +
+        '<input type="text" placeholder="enter URL here"/>' +
+        '<button>Submit</button>' +
+        '<div class="saved-links">Saved Links:</div>' +
+      '</div>'
+  );
+  $(target).siblings('.folder-details').toggle();
+  // $(target).siblings('.folders-list').toggle();
 }
