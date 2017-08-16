62f2483ff10a70e362dd49918c4773dc01edee08
Justyna Field <justyna.field@gmail.com>
2017-08-15 19:52:31 -0600

    create remaining routes for the application

--- server.js
+++ server.js
@@ -7,37 +7,38 @@ const fs = require('fs');
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'Jet Fuel'
 
-//we tell express to use our static files:
+//connecting static files (in the public folder) to the server
 app.use(express.static('public'))
 
 //bodyParser gives an ability to parse the body of an HTTP request
 app.use(bodyParser.json())
+//for url encoded bodies:
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //storing folders:
-app.locals.folders = {
-  wowow: 'folder'
-}
+app.locals.folders = {};
 
-//connecting static files (in the public folder) to the server
+//storing links:
+app.locals.links = {};
+
+//creating a GET request for the HOME PAGE (index.html):
 app.get('/', (request, response) => {
   //__dirname is a global variable that provides the directory name of the current module (the path)
   response.sendFile(__dirname + '/index.html')
 })
 
-//creating a get request for the home page
-app.get('/', (request, response) => {
-  response.send('Server working!')
-})
 
-//creating a GET route for the list of folders:
-app.get('/api.folders', (request, response) => {
+//creating a GET request for the LIST OF FOLDERS:
+app.get('/api/folders', (request, response) => {
   const folders = app.locals.folders
 
-  //this is a shorthand for setting the response type as application/json; server stringifies the response for us
+  //this is a shorthand for setting the response type as application/json
+  //server stringifies the response for us
   response.json({ folders })
 })
 
-//specifying a get request for a specific folder:
+
+//specifying a GET request for a SPECIFIC FOLDER:
 app.get('/api/folders/:id', (request, response) => {
   const { id } = request.params;
   const message = app.locals.folders[id];
@@ -47,19 +48,53 @@ app.get('/api/folders/:id', (request, response) => {
   response.json({ id, message })
 })
 
-//creating a defined POST route
+//************************************************************
+//GET request for all LINKS:
+app.get('/api/links', (request, response) => {
+  const links = app.locals.links
+
+  if(!links) { response.status(404).send('Links not found')}
+
+  response.json({ links })
+})
+
+//GET: requesting all LINKS OF A SPECIFIC FOLDER:
+app.get('/api/folders/:id/links', (request, response) => {
+
+  //?????????????????????
+})
+
+//GET: shortened URLs
+app.get('/api/urls', (request, response) => {
+  const shorturls = app.locals.urls
+  response.json({ shorturls })
+})
+
+//POSTing LINKS
+app.post('/api/links', (request, response) => {
+  const id = Date.now()
+  const { link } = request.body
+
+  app.locals.links[id] = link;
+  response.status(201).json({ id, link })
+})
+
+//*************************************************************
+
+
+//creating a defined POST route for POSTING A FOLDER:
 app.post('/api/folders', (request, response) => {
   //generating a random ID:
   const id = Date.now()
-  const { message } = request.body
+  const { folder } = request.body
 
-  //error handling: if there is no message included in the body, the server will respond with 422 error code (unprocessable entity), and send a notice to include message
-  if (!message) { return response.status(422), send('Must include message in the body')}
+  //error handling: if there is no folder included in the body, the server will respond with 422 error code (unprocessable entity), and send a notice to include folder
+  if (!folder) { return response.status(422), send('Must include folder in the body')}
 
-  app.locals.folders[id] = message;
+  app.locals.folders[id] = folder;
 
   //the response will come with 201 (fulfilled request) code and will post a new folder in json format
-  response.status(201).json({ id, message })
+  response.status(201).json({ id, folder })
 })
 
 
