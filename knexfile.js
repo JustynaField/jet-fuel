14eb123827fbb771ce2aad081d968d35c0bcc76c
Justyna Field <justyna.field@gmail.com>
2017-08-18 11:27:49 -0600

    add test environment

--- knexfile.js
+++ knexfile.js
@@ -10,11 +10,24 @@ module.exports = {
   },
 
   production: {
-  client: 'pg',
-  connection: process.env.DATABASE_URL + `?ssl=true`,
-  migrations: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
     directory: './db/migrations'
   },
+
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/jetfueltest',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/test/seeds'
+    }
+  },
+
   useNullAsDefault: true
 }
 
